2649034eb14166c949437eb2d34c6ed96f39564f
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <203292774+NoxxOP@users.noreply.github.com>
2026-09-19 21:49:06 +0530

    New update [ Inner Core and UI ]

--- ShrutixMusic/utils/inline/start.py
+++ ShrutixMusic/utils/inline/start.py
@@ -1,3 +1,4 @@
+from pyrogram.enums import ButtonStyle
 from pyrogram.types import InlineKeyboardButton
 
 import config
@@ -8,9 +9,15 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{nand.username}?startgroup=true"
+                text=_["S_B_1"],
+                url=f"https://t.me/{nand.username}?startgroup=true",
+                style=ButtonStyle.SUCCESS,
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_CHAT,
+                style=ButtonStyle.PRIMARY,
+            ),
         ],
     ]
     return buttons
@@ -22,12 +29,21 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{nand.username}?startgroup=true",
+                style=ButtonStyle.SUCCESS,
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text=_["S_B_6"],
+                url=config.SUPPORT_CHANNEL,
+                style=ButtonStyle.DANGER,
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_CHAT,
+                style=ButtonStyle.PRIMARY,
+            ),
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
