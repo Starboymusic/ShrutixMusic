44d7eae34fff1a69a60959fac9e507862fb1e9c9
Starboymusic <done!>
2026-09-26 21:18:53 +0530

    update userbot.py

--- ShrutixMusic/core/userbot.py
+++ ShrutixMusic/core/userbot.py
@@ -60,7 +60,7 @@ class Userbot(Client):
                 
                 # Try to join support chats
                 try:
-                    await self.one.join_chat("ShrutiBots")
+                    await self.one.join_chat("Star_Boy_96_vibes")
                     await self.one.join_chat("Star_Boy_96_vibes")
                     LOGGER(__name__).info("Assistant 1 joined support chats")
                 except Exception as e:
@@ -111,7 +111,7 @@ class Userbot(Client):
                 await self.two.start()
                 
                 try:
-                    await self.two.join_chat("ShrutiBots")
+                    await self.two.join_chat("Star_Boy_96_vibes")
                     await self.two.join_chat("Star_Boy_96_vibes")
                 except:
                     pass
@@ -139,7 +139,7 @@ class Userbot(Client):
                 await self.three.start()
                 
                 try:
-                    await self.three.join_chat("ShrutiBots")
+                    await self.three.join_chat("Star_Boy_96_vibes")
                     await self.three.join_chat("Star_Boy_96_vibes")
                 except:
                     pass
@@ -166,7 +166,7 @@ class Userbot(Client):
                 await self.four.start()
                 
                 try:
-                    await self.four.join_chat("ShrutiBots")
+                    await self.four.join_chat("Star_Boy_96_vibes")
                     await self.four.join_chat("Star_Boy_96_vibes")
                 except:
                     pass
@@ -193,7 +193,7 @@ class Userbot(Client):
                 await self.five.start()
                 
                 try:
-                    await self.five.join_chat("ShrutiBots")
+                    await self.five.join_chat("Star_Boy_96_vibes")
                     await self.five.join_chat("Star_Boy_96_vibes")
                 except:
                     pass
